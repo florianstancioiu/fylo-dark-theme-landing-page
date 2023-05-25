dae4bcbba7c8383fdb870cac05363a1b7a757052
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2023-05-25 15:23:28 +0300

    Worked on mobile header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,29 @@
 import classes from './Header.module.css';
+import LogoImg from '../images/logo.svg';
 
-const Header = () => {};
+const Header = () => {
+  return (
+    <header className={classes.header}>
+      <div className={classes.wrapper}>
+        <img
+          src={LogoImg}
+          alt='Fylo dark theme landing page logo'
+          className={classes.logo}
+        />
+        <div className={classes.menu}>
+          <a href='#' className={classes['menu-item']}>
+            Features
+          </a>
+          <a href='#' className={classes['menu-item']}>
+            Team
+          </a>
+          <a href='#' className={classes['menu-item']}>
+            Sign In
+          </a>
+        </div>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
