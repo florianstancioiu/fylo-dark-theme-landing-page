1a4b1d7b4b4c4da13f7bb1f2dae3e4715088ee17
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2023-05-25 16:16:25 +0300

    Worked on the Features mobile section

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -1,5 +1,15 @@
 import classes from './Feature.module.css';
 
-const Feature = () => {};
+const Feature = ({ image, title, description }) => {
+  return (
+    <div className={classes.wrapper}>
+      <img src={image} className={classes.image} alt={title} />
+      <div className={classes['text-wrapper']}>
+        <p className={classes.title}>{title}</p>
+        <p className={classes.description}>{description}</p>
+      </div>
+    </div>
+  );
+};
 
 export default Feature;
--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,5 +1,50 @@
 import classes from './Features.module.css';
+import Feature from './Feature';
 
-const Features = () => {};
+const features = [
+  {
+    id: 1,
+    image: '/images/icon-access-anywhere.svg',
+    title: 'Access your files, anywhere',
+    description:
+      'The ability to use a smartphone, tablet, or computer to access your account means your files follow you everywhere.',
+  },
+  {
+    id: 2,
+    image: '/images/icon-security.svg',
+    title: 'Security you can trust',
+    description:
+      '2-factor authentication and user-controlled encryption are just a couple of the security features we allow to help secure your files.',
+  },
+  {
+    id: 3,
+    image: '/images/icon-collaboration.svg',
+    title: 'Real-time collaboration',
+    description:
+      'Securely share files and folders with friends, family and colleagues for live collaboration. No email attachments required.',
+  },
+  {
+    id: 4,
+    image: '/images/icon-any-file.svg',
+    title: 'Store any type of file',
+    description:
+      "Whether you're sharing holidays photos or work documents, Fylo has you covered allowing for all file types to be securely stored and shared.",
+  },
+];
+
+const Features = () => {
+  return (
+    <div className={classes.wrapper}>
+      {features.map((feature) => (
+        <Feature
+          key={feature.id}
+          image={feature.image}
+          title={feature.title}
+          description={feature.description}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default Features;
