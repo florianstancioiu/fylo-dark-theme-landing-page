36f809e3e4326677cab1403dbd94bd93b3e744a0
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2023-05-27 09:15:32 +0300

    Worked on the mobile stay productive section

--- src/components/StayProductive.jsx
+++ src/components/StayProductive.jsx
@@ -1,5 +1,34 @@
 import classes from './StayProductive.module.css';
+import StayProductiveIllustration from '../images/illustration-stay-productive.png';
+import ArrowIcon from '../images/icon-arrow.svg';
 
-const StayProductive = () => {};
+const StayProductive = () => {
+  return (
+    <div className={classes.wrapper}>
+      <div className={classes['image-wrapper']}>
+        <img
+          src={StayProductiveIllustration}
+          className={classes.image}
+          alt='Stay Productive Illustration'
+        />
+      </div>
+      <div className={classes['text-wrapper']}>
+        <p className={classes.title}>Stay productive, wherever you are</p>
+        <p className={classes.description}>
+          Never let location be an issue when accessing your files. Fylo has you
+          covered for all of your file storage needs.
+        </p>
+        <p className={classes.description}>
+          Securely share files and folders with friends, family and colleagues
+          for live collaboration. No email attachments required.
+        </p>
+        <a href='#' className={classes['text-btn']}>
+          <span>See how Fylo works</span>
+          <img src={ArrowIcon} alt='' />
+        </a>
+      </div>
+    </div>
+  );
+};
 
 export default StayProductive;
