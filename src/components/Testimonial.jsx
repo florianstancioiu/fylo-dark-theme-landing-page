664eff5f53806d4ec6725cefea5ee8e936e4b610
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2023-06-01 12:00:05 +0300

    Worked on the mobile testimonials section

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,5 +1,20 @@
 import classes from './Testimonial.module.css';
 
-const Testimonial = () => {};
+const Testimonial = ({ description, avatar, name, title }) => {
+  return (
+    <div className={classes.wrapper}>
+      <p className={classes.text}>{description}</p>
+      <div className={classes['avatar-wrapper']}>
+        <div className={classes['image-wrapper']}>
+          <img src={avatar} className={classes.image} alt={name} />
+        </div>
+        <div className={classes['name-and-title']}>
+          <p className={classes.name}>{name}</p>
+          <p className={classes.title}>{title}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Testimonial;
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,5 +1,44 @@
 import classes from './Testimonials.module.css';
+import Testimonial from './Testimonial';
 
-const Testimonials = () => {};
+const testimonials = [
+  {
+    id: 1,
+    description: `Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has become a well-oiled collaboration machine.`,
+    avatar: `/images/profile-1.jpg`,
+    name: 'Satish Patel',
+    title: 'Founder & CEO, Huddle',
+  },
+  {
+    id: 2,
+    description: `Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has become a well-oiled collaboration machine.`,
+    avatar: `/images/profile-2.jpg`,
+    name: 'Bruce McKenzie',
+    title: 'Founder & CEO, Huddle',
+  },
+  {
+    id: 3,
+    description: `Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has become a well-oiled collaboration machine.`,
+    avatar: `/images/profile-3.jpg`,
+    name: 'Iva Boyd',
+    title: 'Founder & CEO, Huddle',
+  },
+];
+
+const Testimonials = () => {
+  return (
+    <div className={classes.wrapper}>
+      {testimonials.map((testimonial) => (
+        <Testimonial
+          key={testimonial.id}
+          description={testimonial.description}
+          avatar={testimonial.avatar}
+          name={testimonial.name}
+          title={testimonial.title}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default Testimonials;
